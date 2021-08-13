e40a4966bfb4c00a514d1cff366a7c5756832a06
Prakash-16 <goudprakash58@gmail.com>
2021-08-14 00:44:32 +0530

    [Prakash] - Added length equality feature

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,11 +1,32 @@
 package com.bridgelabz.linecomparison;
-
+import java.util.*;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Line Comaprison Computation Program ");
-		int x1=3,x2=7,y1=2,y2=6;
-		double len_of_line = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-	    System.out.println(" The length of line is " + len_of_line);
+		int i,j;
+		int x[] = new int[2];
+		int y[] = new int[2];
+		double len_of_line[] = new double[2];
+		Scanner sc = new Scanner(System.in);
+		for(i=1;i<=2;i++)
+		{
+			System.out.println("Enter coordinate of line "+i);
+			for(j=1;j<=2;j++)
+		    {
+		    	System.out.println(" Enter x-coordiante of point " + j);
+		    	x[j-1]=sc.nextInt();
+		    	System.out.println(" Enter y-coordiante of point " + j);
+		    	y[j-1]=sc.nextInt();
+		    }
+		    //Length of line
+		    len_of_line[i-1] = Math.sqrt(Math.pow(x[1]-x[0],2) + Math.pow(y[1]-y[0],2));	
+		}
+		if(len_of_line[0]==len_of_line[1]) {
+			System.out.println("The lenght of both the lines are equal");
+		}
+		else {
+			System.out.println("The lenght of both the lines are not equal");
+		} 
 	}
 
 }

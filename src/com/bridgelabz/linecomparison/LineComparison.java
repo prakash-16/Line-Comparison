6c5fec85d0d7b1ac83d369654d835b9160bbc4ad
Prakash-16 <goudprakash58@gmail.com>
2021-08-14 00:40:19 +0530

    [Prakash] - Added length calculation of line feature

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,6 +3,9 @@ package com.bridgelabz.linecomparison;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Line Comaprison Computation Program ");
+		int x1=3,x2=7,y1=2,y2=6;
+		double len_of_line = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+	    System.out.println(" The length of line is " + len_of_line);
 	}
 
 }
